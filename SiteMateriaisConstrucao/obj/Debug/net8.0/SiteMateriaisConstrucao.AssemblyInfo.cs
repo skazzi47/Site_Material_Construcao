e7193159933bc59fef29355435d02ac2db6a54e8
Guilherme Mauricio <guilhermemauricio219@gmail.com>
2025-04-23 11:10:56 -0300

    Update

--- SiteMateriaisConstrucao/obj/Debug/net8.0/SiteMateriaisConstrucao.AssemblyInfo.cs
+++ SiteMateriaisConstrucao/obj/Debug/net8.0/SiteMateriaisConstrucao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiteMateriaisConstrucao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1814c92aa8f5a42acf50b5e3815448462122714d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e4a75687dc2720fef83084f9fea48c05cb1f5e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiteMateriaisConstrucao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiteMateriaisConstrucao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
